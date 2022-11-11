31766ba09a11f526ab85ad39c32a6014de0778a8
Aniket  Warhade <aniketwarhade0411@gmail.com>
2022-11-11 14:04:35 +0530

    add sub code

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ int main()
     int a = 50;
     int b = 100;
     int sum = a + b;
+    int diff = a - b;
     printf("sum of two number is %d\n", sum);
+    printf("difference of two number is %d\n", diff);
 }
